d286abf263126a076128d1342c04c2135145b931
Your Name <you@example.com>
2023-06-27 03:02:11 +0500

    updated

--- resources/views/layouts/frontMaster.blade.php
+++ resources/views/layouts/frontMaster.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
-    <link href="{{asset('front-assets/css/style.css')}}" rel="stylesheet" />
+    <link href="{{asset('front-assets/css/style.css?').time()}}" rel="stylesheet" />
 
     <title>@yield('title','Home')</title>
     {{-- <title>Licata Insurance</title> --}}
