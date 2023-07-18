9574bf335de2fe03d88d9ffe9ec68602d2dbf70d
Your Name <you@example.com>
2023-07-18 06:13:21 +0500

    fixes

--- resources/views/front/inner-pages.blade.php
+++ resources/views/front/inner-pages.blade.php
@@ -52,7 +52,7 @@
             </div>
         </section>
 
-        <section class="bi__fourSteps">
+        {{-- <section class="bi__fourSteps">
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 col-sm-12">
@@ -97,7 +97,7 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> --}}
 
         <!-- <section class="bi__faq">
             <div class="container">
